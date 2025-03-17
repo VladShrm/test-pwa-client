3a0e0bd0b139de91b8263dce5ad725ff95eca468
VladShrm <sherem963@gmail.com>
2025-03-17 13:41:54 +0200

    added PWA

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,20 @@ import './App.css'
 import LoginButton from "./components/login.jsx";
 import VerifyButton from "./components/verify.jsx";
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then((registration) => {
+                console.log('Service Worker зареєстровано:', registration);
+            })
+            .catch((error) => {
+                console.log('Помилка реєстрації Service Worker:', error);
+            });
+    });
+}
+
+
 function App() {
     return (
         <div style={{
